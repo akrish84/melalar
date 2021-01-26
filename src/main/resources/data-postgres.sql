cd750fc035bc53da5c8d3e6a93032dfa42a6f5f9
Archana <archuprasad16@gmail.com>
2021-01-25 17:48:28 -0700

    Database schema

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,5 +1,5 @@
 
 
-insert into UserDetail (firstname, lastname, email, passhash, isactive) values ('akhilesh', 'krishnan', 'test@test.com', 'test', 'true') on conflict do nothing;
-insert into UserDetail (firstname, lastname, email, passhash, isactive) values ('archana', 'narasimha prasar', 'test1@test.com', 'test', 'true')  on conflict do nothing;
-insert into UserDetail (firstname, lastname, email, passhash, isactive) values ('atit', 'gaonkar', 'test3@test.com', 'test', 'true')  on conflict do nothing;
+insert into User_Detail (first_name, last_name, email, pass_hash, is_active) values ('akhilesh', 'krishnan', 'test@test.com', 'test', 'true') on conflict do nothing;
+insert into User_Detail (first_name, last_name, email, pass_hash, is_active) values ('archana', 'narasimha prasar', 'test1@test.com', 'test', 'true')  on conflict do nothing;
+insert into User_Detail (first_name, last_name, email, pass_hash, is_active) values ('atit', 'gaonkar', 'test3@test.com', 'test', 'true')  on conflict do nothing;
--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,9 +1,44 @@
 
-CREATE TABLE IF NOT EXISTS UserDetail (
+CREATE TABLE IF NOT EXISTS User_Detail (
     id SERIAL NOT NULL PRIMARY KEY,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL,
-    email varchar(62) NOT NULL UNIQUE,
-    passhash varchar(128) NOT NULL,
-    isActive boolean DEFAULT 'true'
+    first_Name VARCHAR(50) NOT NULL,
+    last_Name VARCHAR(50) NOT NULL,
+    email VARCHAR(62) NOT NULL UNIQUE,
+    pass_hash VARCHAR(128) NOT NULL,
+    is_Active VARCHAR DEFAULT 'true'
 );
+
+CREATE TABLE IF NOT EXISTS Job_Application_Status (
+    id SERIAL NOT NULL PRIMARY KEY,
+    status_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS User_Job_Application_Status (
+    user_id INTEGER REFERENCES user_detail (id) ON DELETE CASCADE,
+    status_Id INTEGER REFERENCES job_application_status (id) ON DELETE CASCADE   
+);
+
+CREATE TABLE IF NOT EXISTS Job_Resume (
+    id SERIAL NOT NULL PRIMARY KEY,
+    file_name VARCHAR(50) NOT NULL,
+    file_path VARCHAR(1000) NOT NULL,
+    file_hash VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Job_Application (
+    id SERIAL NOT NULL PRIMARY KEY,
+    company_name VARCHAR(50) NOT NULL ,
+    position_applied VARCHAR(50),
+    aps_name VARCHAR(50),
+    notes VARCHAR(1000),
+    job_description VARCHAR(5000),
+    date_applied BIGINT,
+    user_id INTEGER REFERENCES user_detail(id) ON DELETE CASCADE,
+    status_Id INTEGER REFERENCES job_application_status(id) ON DELETE CASCADE,
+    resume_id INTEGER REFERENCES job_resume(id) ON DELETE CASCADE  
+ ); 
+
+ CREATE TABLE IF NOT EXISTS Job_Application_Rank (
+     job_application_id INTEGER REFERENCES job_application(id) ON DELETE CASCADE,
+     rank INTEGER NOT NULL
+ );
