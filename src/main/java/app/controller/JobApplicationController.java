052505a466b355d3d85c8f739e9d101e27120800
akrish84 <akrish84@asu.edu>
2021-02-03 13:22:33 -0700

    1. Changing how input is validated. 2. Adding function to handle job application update request

--- src/main/java/app/controller/JobApplicationController.java
+++ src/main/java/app/controller/JobApplicationController.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.exceptions.BadRequestException;
+import app.exceptions.UnauthorizedException;
 import app.model.JobApplication;
-import app.model.requestresponse.JobApplicationRequest;
-import app.model.requestresponse.JobApplicationResponse;
+import app.model.JobApplicationRank;
+import app.model.requestresponse.JobApplicationAddRequest;
+import app.model.requestresponse.JobApplicationAddResponse;
+import app.model.requestresponse.JobApplicationUpdateRequest;
+import app.model.requestresponse.JobApplicationUpdateResponse;
 import app.service.JobApplicationService;
 
 @RestController
@@ -19,20 +25,13 @@ public class JobApplicationController {
 	private JobApplicationService jobApplicationService;
 
 	@PostMapping("/jobapplication")
-	public ResponseEntity<JobApplicationResponse> addJob(@RequestBody JobApplicationRequest jobApplicationRequest) {
+	public ResponseEntity<JobApplicationAddResponse> addJob(@RequestBody JobApplicationAddRequest jobApplicationRequest) {
 		try {
-			Validator validator = new Validator();
-			validator.addNullCheck(jobApplicationRequest.getUserId(), "User Id cannot be null")
-					.addEmptyCheck(jobApplicationRequest.getCompanyName(), "Company Name cannot be empty")
-					.addDefaultValueCheck(jobApplicationRequest.getStatusId(), "Status Id Cannot be 0")
-					.addDefaultValueCheck(jobApplicationRequest.getDateApplied(), "Date Applied can not be empty")
-					.addLoggedInUserCheck(jobApplicationRequest.getUserId(),
-							"Unauthorized action, user should be logged in");
-
-			if (!validator.isValid()) {
-				return new ResponseEntity<JobApplicationResponse>(
-						new JobApplicationResponse(validator.getErrorMessage()), HttpStatus.BAD_REQUEST);
-			}
+			Validator.defaultValueCheck(jobApplicationRequest.getUserId(), "User Id");
+			Validator.defaultValueCheck(jobApplicationRequest.getStatusId(), "Status Id");
+			Validator.defaultValueCheck(jobApplicationRequest.getDateApplied(), "Date Applied Id");
+			Validator.emptyValueCheck(jobApplicationRequest.getCompanyName(), "Company Name");
+			Validator.loggedInUserCheck(jobApplicationRequest.getUserId());
 
 			JobApplication jobApplication = JobApplication.builder()
 					.companyName(jobApplicationRequest.getCompanyName())
@@ -47,24 +46,96 @@ public class JobApplicationController {
 
 			jobApplication = jobApplicationService.addJobApplication(jobApplication);
 			
-			String successResponseMessage = "Job Application added successfully for user: " + jobApplicationRequest.getUserId();
-			JobApplicationResponse response = new JobApplicationResponse(successResponseMessage);
+			String successResponseMessage = "Job Application added successfully for user: "
+					+ jobApplicationRequest.getUserId() + " application name: "
+					+ jobApplicationRequest.getCompanyName();
+			JobApplicationAddResponse response = new JobApplicationAddResponse(successResponseMessage);
 			
 			System.out.println(successResponseMessage);
 			
-			return new ResponseEntity<JobApplicationResponse>(response, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			String errorMessage = "Failed to add job application";
-			if (jobApplicationRequest != null) {
-				errorMessage = "Failed to add job application for: " + jobApplicationRequest.getUserId();
-			}
+			return new ResponseEntity<JobApplicationAddResponse>(response, HttpStatus.OK);
+		} catch(BadRequestException be) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to add job application: " + be.getMessage();
 			System.out.println(errorMessage);
-			
-			JobApplicationResponse response = new JobApplicationResponse(errorMessage);
-			return new ResponseEntity<JobApplicationResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			be.printStackTrace();
+			JobApplicationAddResponse response = new JobApplicationAddResponse(errorMessage);
+			return new ResponseEntity<JobApplicationAddResponse>(response, HttpStatus.BAD_REQUEST);
+		} catch(UnauthorizedException ue) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to add job application: " + ue.getMessage();
+			System.out.println(errorMessage);
+			ue.printStackTrace();
+			JobApplicationAddResponse response = new JobApplicationAddResponse(errorMessage);
+			return new ResponseEntity<JobApplicationAddResponse>(response, HttpStatus.UNAUTHORIZED);
+		} catch (Exception e) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to add job application: " + e.getMessage();
+			System.out.println(errorMessage);
+			e.printStackTrace();
+			JobApplicationAddResponse response = new JobApplicationAddResponse(errorMessage);
+			return new ResponseEntity<JobApplicationAddResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-
 	}
+	
+	@PutMapping("/jobapplication")
+	public ResponseEntity<JobApplicationUpdateResponse> updateJob(@RequestBody JobApplicationUpdateRequest jobApplicationUpdateRequest) {
+		try {
+			
+			Validator.defaultValueCheck(jobApplicationUpdateRequest.getUserId(), "User Id");
+			Validator.defaultValueCheck(jobApplicationUpdateRequest.getStatusId(), "Status Id");
+			Validator.defaultValueCheck(jobApplicationUpdateRequest.getDateApplied(), "Date Applied Id");
+			Validator.defaultValueCheck(jobApplicationUpdateRequest.getRank(), "Rank Id");
+			Validator.emptyValueCheck(jobApplicationUpdateRequest.getCompanyName(), "Company Name");
+			Validator.loggedInUserCheck(jobApplicationUpdateRequest.getUserId());
+
+			
+			JobApplication jobApplication = JobApplication.builder()
+					.companyName(jobApplicationUpdateRequest.getCompanyName())
+					.positionApplied(jobApplicationUpdateRequest.getPositionApplied())
+					.apsName(jobApplicationUpdateRequest.getApsName())
+					.notes(jobApplicationUpdateRequest.getNotes())
+					.jobDescription(jobApplicationUpdateRequest.getJobDescription())
+					.dateApplied(jobApplicationUpdateRequest.getDateApplied())
+					.userId(jobApplicationUpdateRequest.getUserId())
+					.statusId(jobApplicationUpdateRequest.getStatusId())
+					.build();
+			
+			JobApplicationRank jobApplicationRank = JobApplicationRank.builder()
+					.jobApplicationId(jobApplicationUpdateRequest.getId()).build();
+			
+			
+			
+			String successResponseMessage = "Job Application updated successfully for user: "
+					+ jobApplicationUpdateRequest.getUserId() + " application name: "
+					+ jobApplicationUpdateRequest.getCompanyName();
+			System.out.println(successResponseMessage);
+			
+			JobApplicationUpdateResponse response = new JobApplicationUpdateResponse(successResponseMessage);
+			return new ResponseEntity<JobApplicationUpdateResponse>(response, HttpStatus.OK);
+		} catch(BadRequestException be) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to update job application: " + be.getMessage();
+			System.out.println(errorMessage);
+			be.printStackTrace();
+			JobApplicationUpdateResponse response = new JobApplicationUpdateResponse(errorMessage);
+			return new ResponseEntity<JobApplicationUpdateResponse>(response, HttpStatus.BAD_REQUEST);
+		} catch(UnauthorizedException ue) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to update job application: " + ue.getMessage();
+			System.out.println(errorMessage);
+			ue.printStackTrace();
+			JobApplicationUpdateResponse response = new JobApplicationUpdateResponse(errorMessage);
+			return new ResponseEntity<JobApplicationUpdateResponse>(response, HttpStatus.UNAUTHORIZED);
+		} catch (Exception e) {
+			// add loggedin user id to message
+			String errorMessage = "Failed to update job application: " + e.getMessage();
+			System.out.println(errorMessage);
+			e.printStackTrace();
+			JobApplicationUpdateResponse response = new JobApplicationUpdateResponse(errorMessage);
+			return new ResponseEntity<JobApplicationUpdateResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 
 }
